eae9293d3d14737d92468d6e29ff82d370e62554
Samuel Caetano <sam.kaetano@gmail.com>
2017-07-01 12:52:56 -0300

    Update classifier2.py

--- classifier2.py
+++ classifier2.py
@@ -1,4 +1,4 @@
-# Script baseado em https://blog.keras.io/building-powerful-image-
+# Script inspirado e baseado em https://blog.keras.io/building-powerful-image-
 #classification-models-using-very-little-data.html
 
 # Classificador de cachorros e gatos
@@ -13,6 +13,7 @@ import cv2 as cv
 import numpy as np
 import os
 
+# Monta a rede
 def build_in():
     model = Sequential()
 
@@ -50,20 +51,21 @@ if K.image_data_format() == 'channels_first':
 else:
     input_shape = (img_width, img_height, 3)
     
+# Verifica se a rede ja foi treinada. Se sim, entao deve haver um arquivo que
+# armazena os pesos calculados. Senao, calcula os pesos.
 if os.path.isfile('models/first_try.h5'):
-    # Teste
-    for i in range(1, 16):
-        
-        model = build_in()
-        
+    # Se a rede ja foi treinada anteriormente, entao testa:
+    for i in range(1, 16): # percorre as 15 imagens de teste
+        model = build_in() 
+        model.load_weights('models/first_try.h5') # carrega na rede os pesos lidos do arquivo first_try.h5
         img = image.load_img('images/teste%d.jpg' % i, target_size = (150, 150))
         img = np.expand_dims(img, axis=0)
-        prediction = model.predict(img)
+        prediction = model.predict(img) # joga a imagem de teste contra a rede
          
         print 'chance de teste%d ser cachorro: %f, de ser gato: %f'\
             % (i, prediction[0][0], 1-prediction[0][0])
 else:
-    model = build_in()
+    model = build_in() # monta
     model.compile(loss = 'binary_crossentropy',
                 optimizer = 'rmsprop',
                 metrics = ['accuracy'])
